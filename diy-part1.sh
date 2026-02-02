42160dbbc70b633eb031b01079865eb80b874216
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-02-02 18:45:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@
 # echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed' >>feeds.conf.default
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
 git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
 
